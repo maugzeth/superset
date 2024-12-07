1adadc1654c7c0327803b96537fb3ca99a008399
maugzeth <119737827+maugzeth@users.noreply.github.com>
2024-12-07 14:42:01 +0200

    added base custom branding config

---
+++ docker/pythonpath_dev/superset_config_docker.py
@@ -0,0 +1,15 @@
+# App name displayed in the UI
+APP_NAME = "Dedsol | Superset"
+
+# App icon/logo path
+# APP_ICON = '/static/assets/images/dedsol-logo.png'
+# FAVICONS = [{"href": "/static/assets/images/dedsol-favicon.png"}]
+
+# Where clicking the logo takes you (None defaults to '/superset/welcome')
+LOGO_TARGET_PATH = 'https://dedsol.xyz'
+
+# Tooltip that appears when hovering over the logo
+LOGO_TOOLTIP = "Dedsol"
+
+# Text that appears to the right of the logo
+LOGO_RIGHT_TEXT = "Analysing data on Radix"
